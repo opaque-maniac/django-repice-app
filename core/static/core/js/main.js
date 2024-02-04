dc5f89356efd2a3bdaf4e4cbff3e150b00caef80
opaque-maniac <mbithimark8@gmail.com>
2024-02-04 16:25:45 +0300

    There is a bug in the mobile stylng

--- core/static/core/js/main.js
+++ core/static/core/js/main.js
@@ -2,12 +2,14 @@
 const menuIcon = document.querySelector('#menu-icon');
 const xIcon = document.querySelector('#x-icon');
 const navbar = document.querySelector('.navbar');
+const header = document.querySelector('.header');
 
 // Event listener for the menu icon
 menuIcon.addEventListener('click', () => {
     menuIcon.classList.add('hidden');
     xIcon.classList.remove('hidden');
     navbar.style.top = '93px';
+    header.style.borderBottom = 'none';
 });
 
 // Event listener for the x icon
@@ -15,4 +17,5 @@ xIcon.addEventListener('click', () => {
     xIcon.classList.add('hidden');
     menuIcon.classList.remove('hidden');
     navbar.style.top = '-100%';
+    header.style.borderBottom = '1px solid #000';
 });
